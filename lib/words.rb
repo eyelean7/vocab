e9a145f9bc35aa78ee84b145661ff3308114fcb8
Ilene Gorski <ilene.gorski@gmail.com>
2017-04-28 09:18:05 -0700

    consolidate some specs and add save, all, and find seingleton methods

--- lib/words.rb
+++ lib/words.rb
@@ -1,11 +1,28 @@
 class Entry
-  attr_reader(:headword, :definitions)
+  @@all_entries = []
+  attr_reader(:headword, :definitions, :id)
   define_method(:initialize) do |attributes|
     @headword = attributes[:headword]
     @definitions =[]
+    @id = @@all_entries.length() + 1
   end
   define_method(:add_definition) do |definition|
     @definitions.push(definition)
+  end
+  define_method(:save) do
+    @@all_entries.push(self)
+  end
+  define_singleton_method(:all) do
+    @@all_entries
+  end
+  define_singleton_method(:find) do |identification|
+    found_word = nil
+    @@all_entries.each() do |entry|
+      if entry.id() == identification.to_i()
+        found_word = entry
+      end
+    end
+    found_word
   end
 end
 
--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -2,18 +2,31 @@ require('rspec')
 require('words')
 
 describe(Entry) do
-  describe('#headword') do
-    it('returns the word') do
-      test_entry = Entry.new({:headword => "defenestrate"})
+  test_definition = "to throw out of a window"
+  test_entry = Entry.new({:headword => "defenestrate"})
+  test_entry.add_definition(test_definition)
+  describe('attr_reader') do
+    it('returns attributes when called') do
       expect(test_entry.headword()).to(eq("defenestrate"))
-    end
-  end
-  describe('#definition') do
-    it('returns the definition') do
-      test_definition = "to throw out of a window"
-      test_entry = Entry.new({:headword => "defenestrate"})
-      test_entry.add_definition(test_definition)
       expect(test_entry.definitions()).to(eq(["to throw out of a window"]))
+      expect(test_entry.id()).to(eq(1))
     end
   end
+  describe('.all') do
+    it('starts empty') do
+      expect(Entry.all()).to(eq([]))
+    end
+  end
+  describe('.find') do
+    it('returns an object with a given id') do
+      test_entry.save()
+      expect(Entry.find(test_entry.id())).to(eq(test_entry))
+    end
+  end
+  # describe('#save') do
+  #   it('retains entry') do
+  #     test_entry.save()
+  #     expect(Entry.all()).to(eq(["defenestrate", ["to throw out of a window"]]))
+  #   end
+  # end
 end
